7284d970de993751ad032e80eb4cddec557b2291
Timilehin Akinyemi <akinyemitimilehin044@gmail.com>
2023-02-21 13:58:41 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print all numbers inputted to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+		else
+		{
+			for (i = n; i >= 98; i--)
+			{
+				printf("%d", i);
+				if (i != 98)
+					printf(", ");
+			}
+			printf("\n");
+		}
+}
