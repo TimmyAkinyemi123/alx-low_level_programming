81841e3551a6c66e27dc64afa274e5e3bb5d2a3a
Timilehin Akinyemi <akinyemitimilehin044@gmail.com>
2023-02-21 14:22:14 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,25 +8,16 @@
 
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
+		while (n > 98)
+			printf("%d", n--);
+		printf("%d\n", n);
 	}
-		else
-		{
-			for (i = n; i >= 98; i--)
-			{
-				printf("%d", i);
-				if (i != 98)
-					printf(", ");
-			}
-			printf("\n");
-		}
+	else
+	{
+		while (n < 98)
+			printf("%d", n++);
+		printf("%d\n", n);
+	}
 }
