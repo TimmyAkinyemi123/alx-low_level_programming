65e00165309a59111ed7ef29373c63bb7fbc0b40
Timilehin Akinyemi <akinyemitimilehin044@gmail.com>
2023-02-23 01:47:28 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * multiply - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b, or 0 if either input is 0
+ */
+int mul(int a, int b)
+{
+	int product;
+	int sign;
+
+	if (a == 0 || b == 0)
+		return (0);
+
+	product = 0;
+	sign = ((a < 0) ^ (b < 0)) ? -1 : 1;
+
+	a = (a < 0) ? -a : a;
+	b = (b < 0) ? -b : b;
+
+	while (b--)
+		product += a;
+
+	return (sign * product);
+}
