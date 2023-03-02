14eda9bb5fcba32e09bf5e3607ad1a4a44bacdf3
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-03-02 14:48:11 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,42 +1,50 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_buffer - to print characters
- * @b: character
- * @size: size of buffer
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int o, j, i;
+
+	o = 0;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	for (i = 0; i < size; i += 10)
+	while (o < size)
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10; j++)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if (i + j < size)
-				printf("%02x ", b[i + j]);
+			if (i < j)
+				printf("%02x", *(b + o + i));
 			else
-				printf("   ");
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
 		}
-	}
-	printf(" ");
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
 
-	for (j = 0; j < 10; j++)
-	{
-		if (i + j < size)
-		{
-			if (b[i + j] >= 32 && b[i + j] <= 126)
-				printf("%c", b[i + j]);
-			else
-				printf(".");
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
+		printf("\n");
+		o += 10;
 	}
-	printf("\n");
 }
