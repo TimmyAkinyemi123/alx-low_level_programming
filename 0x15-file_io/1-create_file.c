9b77285dc1ccb62ba9cfcbfec7580c49bdf998e0
Timmy Akinyemi <akinyemitimilehin044@gmail.com>
2023-04-10 12:40:06 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(file, text_content, len);
 
-	if (file == 1 || w == -1)
+	if (file == -1 || w == -1)
 		return (-1);
 	close(file);
 	return (-1);
