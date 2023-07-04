2baed4dc6d134f6ea6e36acb013f64c479b7f119
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-07-04 12:40:10 -0700

    task6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,25 @@
- global main
-extern printf
+section .data
+    format db "Hello, Holberton", 0
+    newline db 10, 0
+
+section .text
+    extern printf
+
+global main
 main:
-mov edi, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
-format: db `Hello, Holberton\n`,
+    sub rsp, 8            ; Align stack pointer
+
+    mov rdi, format       ; Load format string address
+    xor eax, eax          ; Clear EAX register
+    call printf           ; Call printf function
+
+    mov rdi, newline      ; Load newline address
+    xor eax, eax          ; Clear EAX register
+    call printf           ; Call printf function
+
+    add rsp, 8            ; Restore stack pointer
+    mov eax, 0x60         ; System call number for exit
+    xor edi, edi          ; Exit code 0
+    syscall              ; Call kernel to exit
+
+section .data
