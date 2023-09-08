f03bcee11a775d76fb81054171e5a24898c55501
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-09-08 13:19:49 -0700

    so i can merge

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "hash_tables.h"
-
 /**
  * hash_table_create - Creates a hash table
  * @size: The size of the array
@@ -8,28 +7,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
 
-    if (size <= 0)
-        return (NULL);
+	if (size <= 0)
+		return (NULL);
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
-
-    /* Allocate memory for the array of hash_node_t pointers */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
-
-    new_table->size = size;
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
-
-    return (new_table);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	new_table->size = size;
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	return (new_table);
 }
