58dac942e5478c459d31b686972f6537c8460d17
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-09-09 13:38:46 -0700

    task 7

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -110,7 +110,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	return (1); 
+	return (1);
 }
 
 /**
