5acf4989eb876f3d118effe740b0131b4b20f321
Timmy <akinyemitimilehin044@gmail.com>
2024-01-17 02:31:49 -0800

    task 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches a value in an array using ~ algorithm
+ * @array: the areay to he searched
+ * @size: size of the areay;
+ * @value: value to be found
+ * Return: index of value or 1- if not found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0, high = size - 1;
+	size_t pos;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		if (array[high] - array[low] == 0)
+		{
+			if (value == array[low])
+			{
+				printf("Value checked array[%lu] = [%d]\n",
+						(unsigned long)low, array[low]);
+				return (low);
+			}
+			else
+			{
+				printf("Value checked array[%lu] is out of range\n",
+						(unsigned long)high);
+				return (-1);
+			}
+		}
+		pos = low + (((double)(high - low) / (
+						array[high] - array[low])) * (value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+	printf("Value checked array[%lu] is out of range\n", (unsigned long)high);
+	return (-1);
+}
